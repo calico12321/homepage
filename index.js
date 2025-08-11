19783ad5fdfd9fe134db0cf6d766e689b345027d
danpaul <pt2323@gmail.com>
2025-08-11 13:10:31 +0200

    test1

---
+++ index.js
@@ -0,0 +1,16 @@
+const { BlogGen } = require("blog-gen");
+
+const CONTENT_ROOT = __dirname;
+const DIST_ROOT = __dirname + "/dist";
+
+(async () => {
+  const blogGen = await BlogGen.Construct({
+    site: { title: "Calico", description: "", author: "", keywords: [] },
+    build: {
+      contentRoot: CONTENT_ROOT,
+      distRoot: DIST_ROOT,
+    },
+  });
+
+  await blogGen.run();
+})();
